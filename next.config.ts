f869a356753f38706ff992350705ea60768d281f
Kukwa <rickdomijohn1@gmail.com>
2026-01-21 19:43:48 +0800

    refactor: clean up next.config.ts by removing unused environment variables

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
-import type { NextConfig } from "next";
+  import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  env: {
-    NEXT_PUBLIC_APPWRITE_ENDPOINT: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT,
-    NEXT_PUBLIC_APPWRITE_PROJECT_ID: process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID,
-    NEXT_PUBLIC_APPWRITE_DATABASE_ID: process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID,
-  },
+  
   // Allow mobile devices on same network to access dev server
   allowedDevOrigins: ['192.168.*.*', '10.*.*.*', '172.16.*.*'],
 };
