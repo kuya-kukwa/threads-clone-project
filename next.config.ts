20ef66ae8ab39c811c566cdb512103871cc5f392
Kukwa <rickdomijohn1@gmail.com>
2026-01-21 22:53:36 +0800

    Add Appwrite domain to Next.js image remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,24 @@ const nextConfig: NextConfig = {
   
   // Allow mobile devices on same network to access dev server
   allowedDevOrigins: ['192.168.*.*', '10.*.*.*', '172.16.*.*'],
+  
+  // Allow external images from Appwrite Storage
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'fra.cloud.appwrite.io',
+        port: '',
+        pathname: '/v1/storage/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.cloud.appwrite.io',
+        port: '',
+        pathname: '/v1/storage/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
